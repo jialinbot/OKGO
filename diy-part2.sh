9328357f7e92f54a696e827d810cde377c81c187
jialinbot <57110097+jialinbot@users.noreply.github.com>
2022-02-12 10:18:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 
 diy-part2.sh
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.11/g' package/base-files/files/bin/config_generate
 
 # 全能推送
 git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
